a34361302066ee2377a2dec9039626815785a50f
Martin-Pavek <146820572+Martin-Pavek@users.noreply.github.com>
2024-07-08 13:10:52 +0200

    Add files via upload

---
+++ include_3_recurse_1.ps1
@@ -0,0 +1,14 @@
+﻿cls
+$cesta = "C:\Users\DELL\Documents\ps1\include\"
+#$cesta = "R:\"
+#$pole_include = @("*.txt", "*.doc", "*.bas")
+$pole_include = @("*.png")
+
+echo "----------------------------------------------------------------------------------------------------"
+
+Get-ChildItem  -Recurse -Path $cesta -Include $pole_include -Name # takze zpracuje promennou typu pole
+
+echo "----------------------------------------------------------------------------------------------------------"
+
+Get-ChildItem  -Recurse -Path $cesta  -Name # bere vsechny soubory (bez include)
+
